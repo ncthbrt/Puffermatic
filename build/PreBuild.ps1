7a571a39dc20987f1539dc3046377c1354f9f51f
ncthbrt <nick@cuthbert.co.za>
2016-07-12 22:33:52 +0200

    Updated PreBuildScript to fallback onto ikvm if java not installed

--- build/PreBuild.ps1
+++ build/PreBuild.ps1
@@ -1,4 +1,17 @@
 Import-Module "$PSScriptRoot/Get-RelativePath.psm1"
+function JavaInstalled (){
+        try{
+            java -version 2>&1 | out-null
+            return $true;
+        }
+        catch
+        {
+            return $false;
+        }    
+}
+
+
+
 
 $tmp=Get-Location;
 Set-Location "$PSScriptRoot/.."
@@ -12,13 +25,18 @@ Get-ChildItem "./" -Directory |
 Foreach-Object {
     Get-ChildItem $_.FullName -Filter *.g4 -Recurse |
     Foreach-Object {        
-        $relativePath=Get-RelativePath -Folder $parent -FilePath $_.FullName               
-        cmd.exe /c "java -jar $PSScriptRoot/tools/antlr4-csharp-4.5.3-SNAPSHOT-complete.jar -visitor -Dlanguage=CSharp_v4_5  -o ..\gen -package Puffermatic.Grammar ./$relativePath"      
+        $relativePath=Get-RelativePath -Folder $parent -FilePath $_.FullName
+        if(JavaInstalled){            
+            java -jar $PSScriptRoot/tools/antlr4-csharp-4.5.3-SNAPSHOT-complete.jar -visitor -Dlanguage=CSharp_v4_5  -o ..\gen -package Puffermatic.Grammar ./$relativePath
+        }else{                        
+            Write-Warning "Java is not installed. Falling back to IKVM, however this will affect build times"
+            cmd.exe /c "$PSScriptRoot/tools/ikvm.exe -jar $PSScriptRoot/tools/antlr4-csharp-4.5.3-SNAPSHOT-complete.jar -visitor -Dlanguage=CSharp_v4_5  -o ..\gen -package Puffermatic.Grammar ./$relativePath"
+        }                                     
     }
-    $name=Join-Path "../gen/" $_.Name;
-    Write-Output $name          
+    $name=Join-Path "../gen/" $_.Name;              
     if(Test-Path -PathType Container -Path "$name"){    
         Copy-Item ./project.json.template "$name/project.json"
     }
 }
 Set-Location $tmp;
+Write-Output "Finished"
